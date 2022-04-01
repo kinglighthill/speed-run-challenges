f69ee398544ee9e43d1f6e51a20b037f82dbdef0
kinglighthill <kingholyhill@gmail.com>
2022-04-01 18:01:05 +0100

    changes

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -14,7 +14,9 @@ contract Staker {
 
   event Stake(address by, uint256 amount);
 
-  uint256 public deadline = block.timestamp + 30 seconds;
+  uint256 public deadline = block.timestamp + 300 seconds;
+
+  bool public openForWithdraw = false;
 
   constructor(address exampleExternalContractAddress) public {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
@@ -31,22 +33,41 @@ contract Staker {
 
   // After some `deadline` allow anyone to call an `execute()` function
   //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+
   function execute() public {
     if (block.timestamp > deadline) {
-      exampleExternalContract.complete{value: address(this).balance}();
+      if (address(this).balance > threshold) {
+        exampleExternalContract.complete{value: address(this).balance}();    
+      } else if (address(this).balance < threshold) {
+        openForWithdraw = true;
+      }
     }
   }
 
-  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
-
-
   // Add a `withdraw()` function to let users withdraw their balance
-
+  function withdraw() public {
+    if (openForWithdraw) {
+      uint256 amount = balances[msg.sender];
+      // address(this).balance += amount;
+    }
+  }
 
   // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-
+  function timeLeft() public view returns(uint256) {
+    if (block.timestamp >= deadline) {
+      return 0;
+    }
+    return deadline - block.timestamp;
+  }
 
   // Add the `receive()` special function that receives eth and calls stake()
+  receive() external payable {
+    stake();
+  }
 
-
+  modifier notCompleted() {
+    require(exampleExternalContract.completed == false, "Not Completed"); 
+    _;
+  }
 }
